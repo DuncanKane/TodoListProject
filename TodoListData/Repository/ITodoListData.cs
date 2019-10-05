eab927a2806be05da4a72354e394a8003cb53fd5
Duncan <duncankane77@gmail.com>
2019-10-05 10:40:27 +0100

    Data Repo 0.1

---
+++ TodoListData/Repository/ITodoListData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListData.Repository
+{
+    interface ITodoListData<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Add(T todoList);
+        void Update(T todoList);
+        void Delete(T todoList);
+    }
+}
---
+++ TodoListData/Repository/TodoListData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListData.Repository
+{
+    class TodoListData<T> : ITodoListData<T>
+    {
+        public void Add(T todoList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T todoList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T todoList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
