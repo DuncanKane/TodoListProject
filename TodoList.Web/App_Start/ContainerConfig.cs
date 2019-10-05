f32dbbc6f46c4a1e06218a24f494cd6ffe028511
Duncan <duncankane77@gmail.com>
2019-10-05 13:24:44 +0100

    global, conatiner config 0.1

---
+++ TodoList.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TodoListData.Context;
+using TodoListData.Repository;
+
+namespace TodoList.Web.App_Start
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //builder.RegisterType<TodoListData.Repository.TodoListData>().As<TodoListData>().InstancePerRequest();
+            builder.RegisterType<TodoListContext>().InstancePerRequest();
+        }
+    }
+}
--- TodoList.Web/Global.asax.cs
+++ TodoList.Web/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TodoList.Web.App_Start;
 
 namespace TodoList.Web
 {
@@ -16,6 +17,7 @@ namespace TodoList.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ContainerConfig.RegisterContainer();
         }
     }
 }
